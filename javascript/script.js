ad7e07a3bf1c1e1eefe4380884277d87c892879a
kamisato.kira <175933830+cyruschuikc@users.noreply.github.com>
2026-04-29 18:49:59 +0800

    Create script.js

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+const doctitle="🍃KamiKiCy💠|©️by [GitHub]cyruschuikc";
+
+export var largs = new Array ("Title configerd:");
+
+export var mugs = new Array (
+"♤ƙą?ķįČ̣¤_#-#●]÷=ßø,!)#;ł[ɣ■#of#■Ç=÷!þ>:>/_●#&#¤Ð>%>ŧ!·¤#○Ĉ<)/<=÷○",
+doctitle
+);
