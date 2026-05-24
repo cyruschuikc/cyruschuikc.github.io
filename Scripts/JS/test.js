a38c67eee185fbcd5e403419af95d85bfd861f4c
kamisato.kira <175933830+cyruschuikc@users.noreply.github.com>
2026-05-24 17:23:44 +0800

    Update test.js

--- Scripts/JS/test.js
+++ Scripts/JS/test.js
@@ -4,26 +4,13 @@ Author:  cyruschuikc
 Source:  https://github.com/cyruschuikc/cyruschuikc.github.io/edit/main/Scripts/JS/test
 License:  CC BY-NC 4.0 — https://creativecommons.org/licenses/by-nc/4.0/
 **/
-
-const imgFavs = [
-  [document.getElementById("fimg1"), document.getElementById("fimg2")],
-  [document.getElementById("fimg1").getAttribute('alt'), document.getElementById("fimg2").getAttribute('alt')],
-  [document.getElementById("fimg1").getAttribute('src'), document.getElementById("fimg2").getAttribute('src')]
-];
-const alts = [imgFavs[1][0].toString(), imgFavs[1][1].toString()];
-const srcs = [imgFavs[2][0].toString(), imgFavs[2][1].toString()];
-const msgs = [
-  [
-    "'" + alts[0] + "' loaded successful!!!",
-    "'" + alts[1] + "' loaded sucessful!!!"
-  ],
-  [
-    "Err.#404(NOT FOUND): '"+alts[0]+"' cannot found / have not exisits!!!\nsrc: "+srcs[0],
-   "Err.#404(NOT FOUND): '"+alts[1]+"' cannot found / have not exisits!!!\nsrc: "+srcs[1]
-  ]
-  ];
-imgFavs[0][0].onload = () => {console.log(msgs[0][0])};
-imgFavs[0][0].onerror = () => {console.log(msgs[1][0])};
-imgFavs[0][1].onload = () => {console.log(msgs[0][1])};
-imgFavs[0][1].onerror = () => {console.log(msgs[1][1])};
-
+function $(fimg1){return document.getElementById("fimg1")};
+function $$(fimg2){return document.getElementById("fimg2")};
+function $1(fimg1){return document.getElementById("fimg1").getAttribute('alt')};
+function $$1(fimg2){return document.getElementById("fimg2").getAttribute('alt')};
+function $2(fimg1){return document.getElementById("fimg1").getAttribute('src')};
+function $$2(fimg2){return document.getElementById("fimg2").getAttribute('src')};
+$().onload = () => {console.log("'" + $1() + "' loaded successful!!!");};
+$().onerror = () => {console.log("Err.#404(NOT FOUND):" + $1() + "' loaded unsuccessful!!!\n" + $1() + " cannot found or have not exitis!!!\nsrc:"+$2());};
+$$().onload = () => {console.log("'" + $$1() + "' loaded successful!!!");};
+$$().onerror = () => {console.log("Err.#404(NOT FOUND):" + $$1() + "' loaded unsuccessful!!!\n" + $$1() + " cannot found or have not exitis!!!\nsrc:"+$$2());};
