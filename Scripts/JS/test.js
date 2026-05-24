4ad4358035ef3eb35c2e57cfb185ddb29a573ff9
kamisato.kira <175933830+cyruschuikc@users.noreply.github.com>
2026-05-24 18:18:41 +0800

    Update test.js

--- Scripts/JS/test.js
+++ Scripts/JS/test.js
@@ -4,6 +4,11 @@ Author:  cyruschuikc
 Source:  https://github.com/cyruschuikc/cyruschuikc.github.io/edit/main/Scripts/JS/test
 License:  CC BY-NC 4.0 — https://creativecommons.org/licenses/by-nc/4.0/
 **/
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange=()=>{xhttp.readyState==4&&xhttp.status==200?xhttp.responseText:xhttp.responseText};
+xhttp.open("GET", "Scripts/text/welstatementMAIN.txt", true);
+xhttp.send();
+
 function $(fimg1){return document.getElementById("fimg1")};
 function $$(fimg2){return document.getElementById("fimg2")};
 function $1(fimg1){return document.getElementById("fimg1").getAttribute('alt')};
