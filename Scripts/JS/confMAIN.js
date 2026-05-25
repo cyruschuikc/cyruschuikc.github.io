6cb1a124cec859c71665f6f6bd00aa2d0c3c4919
kamisato.kira <175933830+cyruschuikc@users.noreply.github.com>
2026-05-25 18:59:03 +0800

    Update confMAIN.js

--- Scripts/JS/confMAIN.js
+++ Scripts/JS/confMAIN.js
@@ -8,11 +8,11 @@ License:  CC BY-NC 4.0 — https://creativecommons.org/licenses/by-nc/4.0/
 document.charset = "UTF-8";  //charter set
 document.title = "#@CCKCs.NET Blog io HK"; //MyWebSite title
 const favTitle = document.title;
-window.onload = () => {console.log("Welcome to '" + document.title + "' !!!");}
-window.onerror = () => {console.log("Err.#404(NOT FOUND): Fail to access " + document.location + "\nSrouces cannot found // the domain/URL is changed // page is not exist");}
+window.onload = () => {console.log("Welcome to '" + document.title + "' !!!");};
+window.onerror = () => {console.log("Err.#404(NOT FOUND): Fail to access " + document.location + "\nSrouces cannot found // the domain/URL is changed // page is not exist");};
 
 var rights = '{"root": "cyruschuikc", "startat": "2026"+"-"+"May"+"-"+"25", "right": "supervisory", "endat": "-"+"-"+"-"}';
-var objR = JSON,parse(rights);
+var objR = JSON.parse(rights);
 document.getElementById("right").innerHTML =
   "&copy;" + favTitle + objR.start + " ~ " + objR.endat + "<br>" +
   "All copyrights of the whole website is belongs to " + onjR.root + ", " + objR.start + " ~ " + objR.endat;
