96d6617312bdee14bd3e04a941d10d8339acb307
kamisato.kira <175933830+cyruschuikc@users.noreply.github.com>
2026-05-25 13:41:50 +0000

    Update confMAIN.js

--- Scripts/JS/confMAIN.js
+++ Scripts/JS/confMAIN.js
@@ -35,8 +35,8 @@ for (i = 0; i < rights.length; i++){
 };
 var txtRight = "All copyrights is belongs to ";
 for (i = 0; i < args.length; i++){
-  if(args[i]!="\"" && args[i]!="{" && args!="}" && args[i]!=" " && args[i]!=",")
-    txtRight += args[i] + " | ";
+  if(args[i]!='"' && args[i]!="{" && args!="}" && args[i]!=" " && args[i]!="," && args[i]!=":")
+    txtRight += args[i];
   if(i==args.length)
     txtRight += "\n";
 }
