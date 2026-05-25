53c26f31d917beb8f6837ff4feec3857d212f5c1
kamisato.kira <175933830+cyruschuikc@users.noreply.github.com>
2026-05-25 14:16:36 +0000

    Update confMAIN.js

--- Scripts/JS/confMAIN.js
+++ Scripts/JS/confMAIN.js
@@ -9,40 +9,11 @@ document.charset = "UTF-8";  //charter set
 document.title = "#@CCKCs.NET Blog io HK"; //MyWebSite title
 const favTitle = document.title;
 window.onload = () => {console.log("Welcome to '" + document.title + "' !!!");};
+
 var rights = '{"root": "cyruschuikc", "startat": "2026-May-25", "right": "supervisory", "parameter": " ~ ", "endat": "---"}';
 var obj = JSON.parse(rights);
-// var args = new Array();
-// var tmp = "";
-// var count = 0;
-// for (i = 0; i < rights.length; i++){
-//   if(rights[i]=="{"||rights[i]=="}"||rights[i]==":"||rights[i]==","){
-//     args[count] = tmp;
-//     count++;
-//     tmp = rights[i];
-//     args[count] = tmp;
-//     count++;
-//     tmp = "";
-//   }
-//   else if(rights[i]==" "){
-//     args[count] = tmp;
-//     count++;
-//     tmp = "";
-//   }
-//   else
-//     if(rights[i]=='"'){
-//       args[count] = tmp;
-//       count++;
-//       tmp = "";
-//     }
-//   else
-//       tmp += rights[i];
-// };
-// var txtRight = "All copyrights is belongs to ";
-// for (i = 0; i < args.length; i++){
-//   if(args[i]=="cyruschuikc"||args[i]=="2026-May-25"||args[i]=="---"||args[i]=="~")
-//     txtRight += args[i] + " ";
-// };
-// txtRight += "\n";
-// document.getElementById("right").innerHTML = txtRight;
 document.getElementById("right").innerHTML = 
-  "&copy;" + "All copyrights belongs to " + obj.root + "|" + obj.startat + obj.parameter + obj.endat + "<br>";
+  "&copy;" + "All copyrights belongs to " + obj.root + " | " + obj.startat + obj.parameter + obj.endat + "<br>";
+
+var contribution = '{"founder": [{"name": "cyruschuikc", "AssignAt": "25th, May, 2026"}]}';
+var obj2 = JSON.parse(contribution);
