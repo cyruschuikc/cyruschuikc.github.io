c4b22e39c3aaa02e9f5d3d04cd0d8653c7bac3da
kamisato.kira <175933830+cyruschuikc@users.noreply.github.com>
2026-05-25 17:47:27 +0800

    Update confMAIN.js

--- Scripts/JS/confMAIN.js
+++ Scripts/JS/confMAIN.js
@@ -7,12 +7,13 @@ License:  CC BY-NC 4.0 — https://creativecommons.org/licenses/by-nc/4.0/
 
 document.charset = "UTF-8";  //charter set
 document.title = "#@CCKCs.NET Blog io HK"; //MyWebSite title
-
 const favTitle = document.title;
+window.onload = () => {console.log("Welcome to '" + document.title + "' !!!");}
+window.onerror = () => {console.log("Err.#404(NOT FOUND): Fail to access " + document.location + "\nSrouces cannot found // the domain/URL is changed // page is not exist");}
 
-window.onload = () => {
-  console.log("Welcome to '" + document.title + "' !!!");
+var rights = '{"root": "cyruschuikc", "startat": "2026"+"-"+"May"+"-"+"25", "right": "supervisory", "endat": "-"+"-"+"-"}';
+function $(right){
+  "&copy;" + favTitle + rights.start + " ~ " + rights.endat + "<br>" +
+  "All copyrights of the whole website is belongs to " + rights.root + ", " + rights.start + " ~ " + rights.endat;
 }
-window.onerror = () => {
-  console.log("Err.#404(NOT FOUND): Fail to access " + document.location + "\nCannot found the srouces // the domain / URL is changed // page is not exist");
-}
+$.innerHTML = $;
