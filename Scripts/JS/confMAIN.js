70b5d2dd13b9fe9748310176c31d7dde76192538
kamisato.kira <175933830+cyruschuikc@users.noreply.github.com>
2026-05-25 19:49:00 +0800

    Update confMAIN.js

--- Scripts/JS/confMAIN.js
+++ Scripts/JS/confMAIN.js
@@ -9,3 +9,26 @@ document.charset = "UTF-8";  //charter set
 document.title = "#@CCKCs.NET Blog io HK"; //MyWebSite title
 const favTitle = document.title;
 window.onload = () => {console.log("Welcome to '" + document.title + "' !!!");};
+var rights = '{"root": "cyruschuikc", "startat": "2026-May-25", "right": "supervisory", "endat": "---"}';
+// var obj = JSON.parse(rights); !!!--ERROR CRASH CATCHED--!!!
+var args = new Array();
+var tmp = "";
+var count = 0;
+for (i = 0; i < rights.length; i++){
+  if(rights[i]!=" "){
+    if(rights[i]!="""){
+      args[count] = tmp;
+      tmp = "";
+      count++;
+    }
+    else{
+      if(rights[i]==""")  
+        tmp += rights[i];
+      else {
+        args[count] = tmp;
+        tmp = "";
+        count++;
+      }
+    }
+  }
+}
