c6b5df923d8fc71b41d8b0a6f4e0bcfba175df0d
kamisato.kira <175933830+cyruschuikc@users.noreply.github.com>
2026-05-25 19:22:21 +0800

    Update confMAIN.js

--- Scripts/JS/confMAIN.js
+++ Scripts/JS/confMAIN.js
@@ -11,7 +11,7 @@ const favTitle = document.title;
 window.onload = () => {console.log("Welcome to '" + document.title + "' !!!");};
 
 var rights = '{"root": "cyruschuikc", "startat": "2026"+"-"+"May"+"-"+"25", "right": "supervisory", "endat": "-"+"-"+"-"}';
-var obj = JSON.parse(rights);
+// var obj = JSON.parse(rights);
 document.getElementById("right").innerHTML =
-  "&copy;" + favTitle + obj.start + " ~ " + obj.endat + "<br>" +
-  "All copyrights of the whole website is belongs to " + onj.root + ", " + obj.start + " ~ " + obj.endat;
+  "&copy;" + favTitle + rights.start + " ~ " + rights.endat + "<br>" +
+  "All copyrights of the whole website is belongs to " + rights.root + ", " + rights.start + " ~ " + rights.endat;
