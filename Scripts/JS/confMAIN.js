60d975c043c58a6d44a5a61ede9dc4c90853a263
kamisato.kira <175933830+cyruschuikc@users.noreply.github.com>
2026-05-26 14:51:44 +0800

    Update confMAIN.js

--- Scripts/JS/confMAIN.js
+++ Scripts/JS/confMAIN.js
@@ -17,11 +17,11 @@ document.getElementById("right").innerHTML =
 
 var menuTXT = '{"*root": [' + 
   '{"tagTXT": "*", "name": "root page", "URL": "https://cyruschuikc.github.io/", "domain": "cyruschuikc.github.io", "protocol": "HTTPS"},' + 
-  '{"tagTXT": "Home", "name": "home page", "URL": "https://cyrus.github.io/index.html", "domain": "cyruschuikc.github.io/index.html", "protocol": "HTTPS"}' + 
+  '{"tagTXT": "Home", "name": "home page", "URL": "https://cyruschuikc.github.io/index.html, "domain": "cyruschuikc.github.io/index.html", "protocol": "HTTPS"}' + 
   '], "other": ['+
   '{"tagTXT": "?", "name": "coming soon...", "URL": "https://github.com/cyruschuikc/", "domain": "github.com/cyruschuikc", "protocol": "HTTPS"}]}';
 var menuscript = JSON.parse(menuTXT);
-var menutable = "<table border='1'><tr><th><a href='" + menuscript["*root"][0].URL + "'>" + menuscript["*root"][0].tagTXT + "</a></th><th><a href='" + menuscript["*root"][1].URL + "'>" + menuscript["*root"][1].tagTXT + "</a></th>" +"</tr></table>";
+var menutable = "<table border='1'><tr><th><a href='" + menuscript["*root"][0].URL + "'>" + menuscript["*root"][0].tagTXT + "</a></th><th><a href='" + menuscript["*root"][1].URL + "'>" + menuscript["*root"][1].tagTXT + "</a></th><th>" + menuscript["other"][0].tagTXT+ "</th>" +"</tr></table>";
 document.getElementById("menu").innerHTML = menutable;
 
 var contribution = '{"founder": [{"name": "cyruschuikc", "AssignAt": "25th, May, 2026"}]}';
