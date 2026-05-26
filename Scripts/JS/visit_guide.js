17f16862fc5dcf10a967b32c95e5bdf46e8b0f72
kamisato.kira <175933830+cyruschuikc@users.noreply.github.com>
2026-05-26 15:36:32 +0800

    Update visit_guide.js

--- Scripts/JS/visit_guide.js
+++ Scripts/JS/visit_guide.js
@@ -1,10 +1,20 @@
 /**
-var menuTXT = '{"*root": [' + 
-  '{"tagTXT": "*", "name": "root page", "URL": "https://cyruschuikc.github.io/", "domain": "cyruschuikc.github.io", "protocol": "HTTPS"},' + 
-  '{"tagTXT": "Home", "name": "home page", "URL": "https://cyruschuikc.github.io/index.html", "domain": "cyruschuikc.github.io/index.html", "protocol": "HTTPS"}' + 
-  '], "other": ['+
-  '{"tagTXT": "?", "name": "coming soon...", "URL": "https://github.com/cyruschuikc/", "domain": "github.com/cyruschuikc", "protocol": "HTTPS"}]}';
-var menuscript = JSON.parse(menuTXT);
+Title:  visit_guide.js
+Author:  cyruschuikc
+Source:  https://github.com/cyruschuikc/cyruschuikc.github.io/edit/main/Scripts/JS/visit_guide
+License:  CC BY-NC 4.0 — https://creativecommons.org/licenses/by-nc/4.0/
+**/
+
+var menuscript = 
+  '{"*$*": [' +
+    '{"atxt": " *(root)* ", "asrc": "https://cyruschuikc.github.io/"},' +
+    '{"atxt": " Home ", "asrc": "https://cyruschuikc.github.io/index"}],' +
+  '"~": [' +
+  '{"lbltxt": "#####", "content": "The more new sections will be add in the future update!\nPlease stay tuned!"}]}';
+
+var menuobj = JSON.parse(menuscript);
+
+/**
 var menutable = "<table border='1'><tr><th><a href='" + menuscript["*root"][0].URL + "'>" + menuscript["*root"][0].tagTXT + "</a></th><th><a href='" + menuscript["*root"][1].URL + "'>" + menuscript["*root"][1].tagTXT + "</a></th><th> " + menuscript["other"][0].tagTXT + menuscript["other"][0].tagTXT + menuscript["other"][0].tagTXT + " (" + menuscript["other"][0].name + ") " + "</th>" +"</tr></table>";
 document.getElementById("menu").innerHTML = menutable;
 **/
