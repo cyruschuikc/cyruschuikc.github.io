d5470411dc9703fb3098d899f8c65ed1e8f4d8f2
kamisato.kira <175933830+cyruschuikc@users.noreply.github.com>
2026-06-02 12:56:03 +0800

    Update confMAIN.js

--- Scripts/JS/confMAIN.js
+++ Scripts/JS/confMAIN.js
@@ -5,19 +5,18 @@ Source:  https://github.com/cyruschuikc/cyruschuikc.github.io/edit/main/Scripts/
 License:  CC BY-NC 4.0 — https://creativecommons.org/licenses/by-nc/4.0/
 **/
 
-document.charset = "UTF-8";  //charter set
 document.title = "#@CCKCs.NET Blog io HK"; //MyWebSite title
 const favTitle = document.title;
 document.getElementById("thTitle").innerHTML = favTitle;
 document.getElementById("welstate-title").innerHTML = "Welcome to visit " + favTitle + "<br>!!!";
-document.getElementById("welstate-content").innerHTML = "'"+favTitle+"' is a individualise creativetie web site page act a blog to write down a life, future carer, intrest, etc. Of a person who dedicated to engaging in IT industry to be a vocational programmer, a software engineer and developer. <br> <br> Also the 'Past', 'Cureent', and the 'Unkownable Wishing Future' ... (--to be continue--)";
+document.getElementById("welstate-content").innerHTML = "'" + favTitle + "' is a individualise creativetie web site page act a blog to write down a life, future carer, intrest, etc. Of a person who dedicated to engaging in IT industry to be a vocational programmer, a software engineer and developer. <br><br> Also the 'Past', 'Cureent', and the 'Unkownable Wishing Future' ... (--to be continue--)";
 
-window.onload = () => {console.log("Welcome to '" + document.title + "' !!!");};
+window.onload = () => { console.log("Welcome to '" + document.title + "' !!!"); };
 
 var rights = '{"root": "cyruschuikc", "startat": "2026-May-25", "right": "supervisory", "parameter": " ~ ", "endat": "---"}';
 var obj = JSON.parse(rights);
-document.getElementById("right").innerHTML = 
-  "&copy;" + "All copyrights belongs to " + obj.root + " | " + obj.startat + obj.parameter + obj.endat + "<br>";
+document.getElementById("right").innerHTML =
+  "&copy; All copyrights belongs to " + obj.root + " | " + obj.startat + obj.parameter + obj.endat + "<br>";
 
 // function of read visitguide.xml
 function readMenuXML(url) {
@@ -28,22 +27,18 @@ function readMenuXML(url) {
       const xmlDoc = parser.parseFromString(str, "application/xml");
       const atxt = xmlDoc.getElementsByTagName("tagtxt");
       const asrc = xmlDoc.getElementsByTagName("href");
-      var menuobj = new Array();
+      let menuobj = [];
       for (let i = 0; i < atxt.length; i++) {
         const text = atxt[i].textContent;
         const link = asrc[i].textContent;
         menuobj.push(`<th><a href="${link}">${text}</a></th>`);
       }
-      var menutxt = "<tr>";
-      for (let i = 0; i < menuobj.length; i++) {
-        menutxt += menuobj[i];
-      }
-      menutxt += "</tr>";
+      const menutxt = `<tr>${menuobj.join("")}</tr>`;
       document.getElementById("menu").innerHTML = `<table border="1">${menutxt}</table>`;
     })
     .catch(err => console.error("read XML failed:", err));
-} readMenuXML("visitguide.xml");
-//
+}
+readMenuXML("visitguide.xml");
 
 // function to read abstract.xml
 function readAbstractXML(url) {
@@ -54,17 +49,17 @@ function readAbstractXML(url) {
       const xmlDoc = parser.parseFromString(str, "application/xml");
       const tvID = xmlDoc.getElementsByTagName("id");
       const tvContent = xmlDoc.getElementsByTagName("content");
-      var abstractobj = new Array();
+      let abstractobj = [];
       for (let i = 0; i < tvID.length; i++) {
         const pTXTid = tvID[i].textContent;
-        const pTXT = tvContent[i].textContent ;
-        abstract.push(`<p id='${pTXTid}'>${pTXT}</p>`);
+        const pTXT = tvContent[i].textContent;
+        abstractobj.push(`<p id="${pTXTid}">${pTXT}</p>`);
       }
       document.getElementById("abstract").innerHTML = abstractobj.join("");
     })
     .catch(err => console.error("read XML failed:", err));
-} readAbstractXML("Resources/xml/abstract.xml");
-//
+}
+readAbstractXML("Resources/xml/abstract.xml");
 
 var contribution = '{"founder": [{"name": "cyruschuikc", "AssignAt": "25th, May, 2026"}]}';
 var obj3 = JSON.parse(contribution);
