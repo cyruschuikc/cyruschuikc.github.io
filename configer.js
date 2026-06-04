28f11925ba5e5168d621aa8007715f2fbd661bf0
kamisato.kira <175933830+cyruschuikc@users.noreply.github.com>
2026-06-04 11:53:27 +0800

    Update configer.js

--- configer.js
+++ configer.js
@@ -1,4 +1,16 @@
+/**
 Title: configer.js
 Author: cyruschuikc
 Source: https://github.com/cyruschuikc/main/configer.js
 License: *CC BY-NC 4.0 — https://creativecommons.org/licenses/by-nc/4.0/
+**/
+
+function readXML(url){
+  fetch(url).then(res => res.text()).then(str => 
+  {
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(str, "application/xml");
+  })
+} 
+
+readXML("");
