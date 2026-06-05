a15a65d346b22062f19b15c5c2912c897578e87a
kamisato.kira <175933830+cyruschuikc@users.noreply.github.com>
2026-06-05 13:10:52 +0800

    Create configer.js

---
+++ configer.js
@@ -0,0 +1,7 @@
+/**
+Title: configer.js
+Author: cyruschuikc
+Source: https://github.com/cyruschuikc/main/configer.js
+License: *CC BY-NC 4.0 — https://creativecommons.org/licenses/by-nc/4.0/
+**/
+
