781e27cf7624cc3d5bae78294e89ea9f8f0f364a
kamisato.kira <175933830+cyruschuikc@users.noreply.github.com>
2026-06-06 16:05:33 +0800

    Update configer.js

--- configer.js
+++ configer.js
@@ -5,3 +5,12 @@ Source: https://github.com/cyruschuikc/main/configer.js
 License: *CC BY-NC 4.0 — https://creativecommons.org/licenses/by-nc/4.0/
 **/
 
+function readXMLBrowser(url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(xmlString => {
+            const parser = new DOMParser(); // Initialize the DOMParser
+            const xmlDoc = parser.parseFromString(xmlString, "text/xml"); // Parse the string into an XML DOM Document
+        })
+        .catch(error => console.error("Error reading XML:", error)); //catch error
+}
