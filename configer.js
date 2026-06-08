a7b3a13bdb9e67d57e33c3fc0033e708530497aa
kamisato.kira <175933830+cyruschuikc@users.noreply.github.com>
2026-06-08 18:32:30 +0800

    Update configer.js

--- configer.js
+++ configer.js
@@ -13,59 +13,89 @@ function readXML(url) {
       const parser = new DOMParser();
       const xmlDoc = parser.parseFromString(xmlString, "application/xml");
 
-    if(url==="Resources/xml/strings.xml"){
-      const strn = xmlDoc.getElementsByTagName("string");
-      const atrn = xmlDoc.getElementsByTagName("author");
-      const cprn = xmlDoc.getElementsByTagName("copyright");
-      const imgn = xmlDoc.getElementsByTagName("image");
-      const imgan = xmlDoc.getElementsByTagName("image_alt");
-      const imgocn = xmlDoc.getElementsByTagName("image_onclick");
-      const strs = new Array();
-      const author = atrn[0].textContent.trim();
-      const cprTXT = cprn[0].textContent.trim();
-      const imgs = new Array();
-      const imgas = new Array();
-      const imgocs = new Array();
-      for(let i=0; i<strn.length; i++){
-        strs.push(strn[i].textContent. trim());
-      }
-      for(let i=0; i<imgn.length; i++){
-        imgs.push(imgn[i].textContent. trim());
-      }
-      for(let i=0; i<imgan.length; i++){
-        imgas.push(imgan[i].textContent.trim());
-      }
-      for(let i=0; i<imgocn.length; i++){
-      imgocs.push(imgocn[i].textContent. trim());
-}
-      document.title = strs[0];
-      document.getElementById("web_name").innerHTML = strs[0];
-      const mDesc = document.querySelector("meta[name='description']");
-      if(mDesc){
-        mDesc.setAttribute("content",  strs[1]);
-      };
-      const mAuthor = document.querySelector("meta[name='author']");
-      mAuthor.setAttribute("content", author);
-      const mCPR = document.querySelector("meta[name='copyright']");
-      mCPR.setAttribute("content", cprTXT);
+      if (url === "Resources/xml/strings.xml") {
+        const strn = xmlDoc.getElementsByTagName("string");
+        const atrn = xmlDoc.getElementsByTagName("author");
+        const cprn = xmlDoc.getElementsByTagName("copyright");
+        const imgn = xmlDoc.getElementsByTagName("image");
+        const imgan = xmlDoc.getElementsByTagName("image_alt");
+        const imgocn = xmlDoc.getElementsByTagName("image_onclick");
 
-      const fav = document.querySelector("link[rel='icon']");
-      fav.setAttribute("type", "image/png");
-      fav.setAttribute("href", imgs[0]);
+        const strs = [];
+        const imgs = [];
+        const imgas = [];
+        const imgocs = [];
 
-      const fav_ = document.querySelector("img[id='site-logo']");
-      fav_.setAttribute("id", "favimg");
-      fav_.setAttribute("name", "favlogo");
-      fav_.setAttribute("src", imgs[0]);
-      fav_.setAttribute("alt", imgas[0]);
-      fav_.setAttribute("onclick", imgocs[0]);
+        for (let i = 0; i < strn.length; i++) {
+          strs.push(strn[i].textContent.trim());
+        }
+        for (let i = 0; i < imgn.length; i++) {
+          imgs.push(imgn[i].textContent.trim());
+        }
+        for (let i = 0; i < imgan.length; i++) {
+          imgas.push(imgan[i].textContent.trim());
+        }
+        for (let i = 0; i < imgocn.length; i++) {
+          imgocs.push(imgocn[i].textContent.trim());
+        }
 
-    }
-  })
-  // catch error
-  .catch(error => {
-    console.error("Error reading XML strings:", error); // error response
-  });
+        const author = atrn.length > 0 ? atrn[0].textContent.trim() : "";
+        const cprTXT = cprn.length > 0 ? cprn[0].textContent.trim() : "";
+
+        // set web title and name
+        if (strs.length > 0) {
+          document.title = strs[0];
+          const webName = document.getElementById("web_name");
+          if (webName) {
+            webName.innerHTML = strs[0];
+          }
+        }
+
+        // set meta description
+        const mDesc = document.querySelector("meta[name='description']");
+        if (mDesc && strs.length > 1) {
+          mDesc.setAttribute("content", strs[1]);
+        }
+
+        // set meta author
+        const mAuthor = document.querySelector("meta[name='author']");
+        if (mAuthor) {
+          mAuthor.setAttribute("content", author);
+        }
+
+        // set meta copyright
+        const mCPR = document.querySelector("meta[name='copyright']");
+        if (mCPR) {
+          mCPR.setAttribute("content", cprTXT);
+        }
+
+        // set favicon
+        let fav = document.querySelector("link[rel='icon']");
+        if (!fav) {
+          fav = document.createElement("link");
+          fav.rel = "icon";
+          document.head.appendChild(fav);
+        }
+        if (imgs.length > 0) {
+          fav.setAttribute("type", "image/png");
+          fav.setAttribute("href", imgs[0]);
+        }
+
+        // set logo img
+        const fav_ = document.querySelector("img[id='site-logo']");
+        if (fav_ && imgs.length > 0) {
+          fav_.setAttribute("id", "favimg");
+          fav_.setAttribute("name", "favlogo");
+          fav_.setAttribute("src", imgs[0]);
+          if (imgas.length > 0) fav_.setAttribute("alt", imgas[0]);
+          if (imgocs.length > 0) fav_.setAttribute("onclick", imgocs[0]);
+        }
+      }
+    })
+    // catch error
+    .catch(error => {
+      console.error("Error reading XML strings:", error); // error response
+    });
 }
 
 readXML("Resources/xml/strings.xml");
