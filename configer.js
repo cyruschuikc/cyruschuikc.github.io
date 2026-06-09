a69c71444b1ed4efa8ecfd08ddfda1e2e4c21be6
kamisato.kira <175933830+cyruschuikc@users.noreply.github.com>
2026-06-09 12:26:53 +0800

    Update configer.js

--- configer.js
+++ configer.js
@@ -56,7 +56,7 @@ const fav = document.querySelector("link[rel='icon']");
 fav.setAttribute("name", "favicon");
 fav.setAttribute("href","Resources/fav/imgFav_standard.png");
 
-const fav_ = document.querySelector("link[rel='icon']");
+const fav_ = document.querySelector("img[id='site-logo]");
 fav_.setAttribute("name", "favlogo");
 fav_.setAttribute("href","Resources/fav/imgFav_standard(revise).png");
 fav_.setAttribute("alt", document.title + "|*");
