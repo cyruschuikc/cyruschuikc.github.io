842247a0006e775e2d2cc54b6761062ea1ae57d2
kazuma.kazuki <175933830+cyruschuikc@users.noreply.github.com>
2026-07-17 20:01:48 +0800

    Update getMainMenu.php

--- application/JQreader/getMainMenu.php
+++ application/JQreader/getMainMenu.php
@@ -1,12 +1,26 @@
 <?php
-    header("Content-Type:Application/json");
-    $siteGuide = array(
-        "page"=>"CCKCs WebSiteBlog GHio",
-        "tab"=>array(
-            array("link"=>"cyruschuikc.github.io", "text"=>"Home", "query"=>"/"),
-            array("link"=>"cyruschuikc.infinityfree.io", "text"=>"&#128271;", "query"=>"/?i=1")
-        )
-    );
+// Handle CORS
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    // Preflight response
+    header("Access-Control-Allow-Origin: https://cyruschuikc.github.io");
+    header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+    header("Access-Control-Allow-Headers: Content-Type");
+    exit(0);
+}
 
-    echo json_encode($siteGuide);
+// Normal response headers
+header("Access-Control-Allow-Origin: https://cyruschuikc.github.io");
+header("Content-Type: application/json");
+
+// Build your menu structure
+$siteGuide = array(
+    "page" => "CCKCs WebSiteBlog GHio",
+    "tab" => array(
+        array("link" => "cyruschuikc.github.io", "text" => "Home", "query" => "/"),
+        array("link" => "cyruschuikc.infinityfree.io", "text" => "🔒", "query" => "/?i=1")
+    )
+);
+
+// Output JSON
+echo json_encode($siteGuide);
 ?>
