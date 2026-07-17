348d67224d535a6faf3d290760aba7a40d000a3f
kazuma.kazuki <175933830+cyruschuikc@users.noreply.github.com>
2026-07-17 18:22:12 +0800

    Update getJSON_MMenu.js

--- res/JScript/getJSON_MMenu.js
+++ res/JScript/getJSON_MMenu.js
@@ -4,35 +4,13 @@ $(document).ready(function(){
     url: '/application/JQreader/getMainMenu.php',
     dataType: 'json',
     success: function(menu){
-      pageTitle = menu.page;
-      hyperlink = menu.tab;
+      const hyperlink = menu.tab;
+      const page = hyperlink.map(h => h.text);
+      const pageUri = hyperlink.map(h => h.link + h.query);
 
-      homepage = hyperlink[0];
-      console.log(homepage);
-
-      otherPages = [];
-      for (let i=1; i<hyperlink.length+1; i++) {
-        otherPages[] = hyperlink[i];
-        console.log(otherPages[i-1]);
-      }
-      console.log(otherPages);
-
-      page = []; pageUri = [];
-      for(i=0; i<hyperlink.length; i++){
-        if(i==0){
-          page[] = homepage.text;
-          console.log(page[0]);
-          pageUri[] = homepage.link + homepage.query;
-          console.log(pageUri[0]);
-        } 
-        else{
-          page[] = otherPages[i].text;
-          console.log(page[i]);
-          pageUri[] = otherPages[i].link + otherPages[i].query;
-          console.log(pageUri[i]);
-        }
-      }
+      // for debug
       console.log(page);  console.log(pageUri);
+      //---
     }
   });
 })
