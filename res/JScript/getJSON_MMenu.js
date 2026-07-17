cd034ffc8d2c91a8f1ecef0bcea2a9e143523ec6
kazuma.kazuki <175933830+cyruschuikc@users.noreply.github.com>
2026-07-17 13:03:30 +0800

    Update getJSON_MMenu.js

--- res/JScript/getJSON_MMenu.js
+++ res/JScript/getJSON_MMenu.js
@@ -6,11 +6,15 @@ $(document).ready(function(){
     success: function(menu){
       pageTitle = menu.page;
       hyperlink = menu.tab;
+
       homepage = hyperlink[0];
-      otherPages = []; otherPageUri = [];
+      console.log(homepage);
+
+      otherPages = [];
       for (i=1, i<hyperlink.length+1; i++){
-        //……
-      }
+        otherPages[] = hyperlink[i];
+        console.log(otherPages[i-1]);
+      } console.log(otherPages);
     }
   });
 })
