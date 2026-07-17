9cdab1a00687dbcca34a5a0a6069f2555acac0ae
kazuma.kazuki <175933830+cyruschuikc@users.noreply.github.com>
2026-07-17 20:50:06 +0800

    Update getMainMenu.php

--- application/JQreader/getMainMenu.php
+++ application/JQreader/getMainMenu.php
@@ -1,6 +1,12 @@
 <?php
 // Handle CORS
-header("Access-Control-Allow-Origin: https://cyruschuikc.github.io");
+$allowed = [
+    "https://cyruschuikc.github.io",
+    "https://cyruschuikc.infinityfree.io"
+];
+if (isset($_SERVER['HTTP_ORIGIN']) && in_array($_SERVER['HTTP_ORIGIN'], $allowed)) {
+    header("Access-Control-Allow-Origin: " . $_SERVER['HTTP_ORIGIN']);
+}
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header("Content-Type: application/json");
