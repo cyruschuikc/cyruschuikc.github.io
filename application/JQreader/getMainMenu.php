1b5981d45fcc48ed88989bece9dabece55ed8cd0
kazuma.kazuki <175933830+cyruschuikc@users.noreply.github.com>
2026-07-17 12:22:20 +0800

    Update getMainMenu.php

--- application/JQreader/getMainMenu.php
+++ application/JQreader/getMainMenu.php
@@ -1,9 +1,12 @@
-<!--
-{
-  "site": "CCKCs WebSiteBlog GHio",
-  "tab":{
-    ["hyperlink": "cyruschuikc.github.io", "text": "Home", "query": "/"],
-    ["hyperlink": "cyruschuikc.infinityfree.io", "text": "&#128271;", "query": "/?i=1"]
-  }
-}
--->
+<?php
+    header("Content-Type:Application/json");
+    $siteGuide = array(
+        "page"=>"CCKCs WebSiteBlog GHio",
+        "tab"=>array(
+            array("link"=>"cyruschuikc.github.io", "text"=>"Home", "query"=>"/"),
+            array("link"=>"cyruschuikc.infinityfree.io", "text"=>"&#128271;", "query"=>"/?i=1")
+        )
+    );
+
+    echo json_encode($siteGuide);
+?>
