3450ea72c1a4c44a1cdade8845ad565a20ae01d1
hoshmisato.shun <175933830+cyruschuikc@users.noreply.github.com>
2026-09-10 12:57:01 +0800

    Update index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -7,8 +7,10 @@ $charset = "utf8mb4";
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_ERRMODE => 
+      PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => 
+      PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES => false,
 ];
 
