1ad1b9f15a0215157cb1325cd0d8b4e5c3b9ed06
hoshmisato.shun <175933830+cyruschuikc@users.noreply.github.com>
2026-09-10 12:57:16 +0800

    Update index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -11,7 +11,8 @@ $options = [
       PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => 
       PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false,
+    PDO::ATTR_EMULATE_PREPARES => 
+      false,
 ];
 
 try {
