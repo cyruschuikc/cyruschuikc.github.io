7a4967589c19162340c79bf74d3a7a4e2141ac86
hoshmisato.shun <175933830+cyruschuikc@users.noreply.github.com>
2026-09-10 17:03:39 +0800

    Update index.php

--- htdocs/index.php
+++ htdocs/index.php
@@ -9,7 +9,7 @@ $conn->set_charset("utf8mb4");
 
 if ($conn->connect_error) {
     error_log("==!!!Connect Error!!!==\nAccess MyWebSite's InfinityFree MySQL Database Rejected.\n" . $conn->connect_error);
-    die("Database connection failed.");
+    die("HSCKdb connection failed.");
 }
 echo "Connect MyWebSite's InfinityFree MySQL Database Successfully!";
 ?>
