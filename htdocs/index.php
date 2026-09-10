99184bc67ffdaf9f077e502310d0fce652de739d
hoshmisato.shun <175933830+cyruschuikc@users.noreply.github.com>
2026-09-10 12:56:17 +0800

    Create index.php

---
+++ htdocs/index.php
@@ -0,0 +1,21 @@
+<?php
+$host = "sql302.infinityfree.com";
+$db = "if0_12345678_HSCKdb";
+$user = "if0_12345678";
+$pass = "cckcCCKC1024";
+$charset = "utf8mb4";
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+    echo "Connect InfinityFree MySQL Database Successfull！";
+} catch (\PDOException $e) {
+    die("InfinityFree MySQL Database connect rejected: " . $e->getMessage());
+}
+?>
