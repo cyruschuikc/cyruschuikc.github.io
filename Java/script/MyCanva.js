697b556455585c8f419f61073bd517f35faeec28
kizuna.sora <175933830+cyruschuikc@users.noreply.github.com>
2026-09-21 12:44:03 +0800

    Update MyCanva.js

--- Java/script/MyCanva.js
+++ Java/script/MyCanva.js
@@ -1,3 +1,6 @@
 const canvas = document.getElementById("welcome-hello");
 const ctx = canvas.getContext("2d");
 const cp = [canvas.width / 2, canvas.height / 2];
+
+ctx.font = '20px Arial';
+ctx.fillStyle = '#1447ff';
