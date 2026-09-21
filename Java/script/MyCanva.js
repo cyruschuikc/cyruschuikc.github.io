3264f4324c3957e4cba7b675b1f1573fcfad3767
kizuna.sora <175933830+cyruschuikc@users.noreply.github.com>
2026-09-21 13:07:46 +0800

    Update MyCanva.js

--- Java/script/MyCanva.js
+++ Java/script/MyCanva.js
@@ -7,13 +7,14 @@ ctx.fillStyle = '#1447ff';
 ctx.textAlign = 'center';
 
 ctx.textBaseline = 'top';
-ctx.fillText("HELLO!", cp[0], cp[1]-20);
-ctx.fillText("Hi!", cp[0]-20, cp[1]+10);
-ctx.fillText("Yea!", cp[0], cp[1]+10);
-ctx.fillText("Hay!", cp[0]+20, cp[1]+10);
+ctx.fillText("HELLO!", cp[0], cp[1] - 20);
+
+ctx.fillText("Hi!", cp[0] - 50, cp[1] + 10);
+ctx.fillText("Yea!", cp[0], cp[1] + 10);
+ctx.fillText("Hay!", cp[0] + 50, cp[1] + 10);
 
 ctx.textBaseline = 'middle';
 ctx.fillText("Y!-O!-Yo!", cp[0], cp[1]);
 
-ctx.textBaseline = 'bottom;
-ctx.fillText("WELCOME!", cp[0], cp[1]+20);
+ctx.textBaseline = 'bottom';
+ctx.fillText("WELCOME!", cp[0], cp[1] + 20);
