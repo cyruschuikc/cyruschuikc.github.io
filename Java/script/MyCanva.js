d8d876f4c08ea743bac1f2b9fe39f1f679135901
kizuna.sora <175933830+cyruschuikc@users.noreply.github.com>
2026-09-21 12:33:01 +0800

    Update MyCanva.js

--- Java/script/MyCanva.js
+++ Java/script/MyCanva.js
@@ -1,4 +1,3 @@
 const canvas = document.getElementById("welcome-hello");
 const ctx = canvas.getContext("2d");
-const centerX = ctx.width / 2;
-const centerY = ctx.height / 2;
+const cp = [ctx.width / 2, ctx.height / 2]
