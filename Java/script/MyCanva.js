894dd8cf3d47f812e1d34ace3b41c8209db1aced
kizuna.sora <175933830+cyruschuikc@users.noreply.github.com>
2026-09-21 12:41:34 +0800

    Update MyCanva.js

--- Java/script/MyCanva.js
+++ Java/script/MyCanva.js
@@ -1,6 +1,3 @@
 const canvas = document.getElementById("welcome-hello");
 const ctx = canvas.getContext("2d");
 const cp = [canvas.width / 2, canvas.height / 2];
-ctx.fillStyle = "black";
-
-ctx.fillRect(cp[0] / 2, cp[1] / 2, canvas.width * 0.75, canvas.height * 0.75);
