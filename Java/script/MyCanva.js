d6cd39d0b4197906e4547a194268840077319060
kizuna.sora <175933830+cyruschuikc@users.noreply.github.com>
2026-09-21 13:06:04 +0800

    Update MyCanva.js

--- Java/script/MyCanva.js
+++ Java/script/MyCanva.js
@@ -4,6 +4,16 @@ const cp = [canvas.width / 2, canvas.height / 2];
 
 ctx.font = '20px Arial';
 ctx.fillStyle = '#1447ff';
+ctx.textAlign = 'center';
 
-ctx.fillText("HELLO!", cp[0], 0);
-ctx.fillText("WELCOME!", cp[0], cp[1]);
+ctx.textBaseline = 'top';
+ctx.fillText("HELLO!", cp[0], cp[1]-20);
+ctx.fillText("Hi!", cp[0]-20, cp[1]+10);
+ctx.fillText("Yea!", cp[0], cp[1]+10);
+ctx.fillText("Hay!", cp[0]+20, cp[1]+10);
+
+ctx.textBaseline = 'middle';
+ctx.fillText("Y!-O!-Yo!", cp[0], cp[1]);
+
+ctx.textBaseline = 'bottom;
+ctx.fillText("WELCOME!", cp[0], cp[1]+20);
