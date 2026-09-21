8ba76c984500f2f7cbea7c8019422e8e26e2be2d
kizuna.sora <175933830+cyruschuikc@users.noreply.github.com>
2026-09-21 13:24:01 +0800

    Update MyCanva.js

--- Java/script/MyCanva.js
+++ Java/script/MyCanva.js
@@ -8,14 +8,14 @@ ctx.textAlign = 'center';
 
 ctx.textBaseline = 'top';
 // top-side text
-ctx.fillText("HELLO!", cp[0], cp[1] - 20);
+ctx.fillText("HELLO!", cp[0], cp[1] - 30);
 // upper-middle-side text
-ctx.fillText("Hi!", cp[0] - 50, cp[1] + 50);
-ctx.fillText("Yea!", cp[0], cp[1] + 50);
-ctx.fillText("Hay!", cp[0] + 50, cp[1] + 50);
+ctx.fillText("Hi!", cp[0] - 30, cp[1] + 30);
+ctx.fillText("Yea!", cp[0], cp[1] + 30);
+ctx.fillText("Hay!", cp[0] + 30, cp[1] + 30);
 // middle side text
 ctx.textBaseline = 'middle';
 ctx.fillText("Y!-O!-Yo!", cp[0], cp[1]);
 // bottom side text
 ctx.textBaseline = 'bottom';
-ctx.fillText("WELCOME!", cp[0], cp[1] + 50);
+ctx.fillText("WELCOME!", cp[0], cp[1] + 30);
